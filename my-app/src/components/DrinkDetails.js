c0bcc31e6807da4bfc85611f78f33df798af3cb3
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-06-13 11:51:16 +0300

    Add create new rating

--- my-app/src/components/DrinkDetails.js
+++ my-app/src/components/DrinkDetails.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import RatingList from './RatingList';
 import RatingForm from './RatingForm';
 
 const DrinkDetails = ({ drink }) => {
+  const [reloadRatings, setReloadRatings] = useState(false);
+
   const handleRatingSubmit = (newRating) => {
-    console.log(newRating);
-    // ...
+    fetch('http://localhost:9292/ratings', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newRating),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setReloadRatings(true);
+      })
+      .catch((error) => {
+        console.error('Error submitting rating:', error);
+      });
   };
+
+  useEffect(() => {
+    if (reloadRatings) {
+      // Fetch updated ratings
+      fetch(`http://localhost:9292/ratings?drink_id=${drink.id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setReloadRatings(false);
+        })
+        .catch((error) => {
+          console.error(`Error fetching ratings for drink ${drink.id}:`, error);
+          setReloadRatings(false);
+        });
+    }
+  }, [drink.id, reloadRatings]);
 
   return (
     <div className="drink-details">
@@ -15,7 +46,7 @@ const DrinkDetails = ({ drink }) => {
       <p>Ingredients: {drink.ingredients}</p>
       <img src={drink.image_url} alt={drink.name} />
 
-      <RatingList drinkId={drink.id} />
+      <RatingList drinkId={drink.id} reloadRatings={reloadRatings} />
 
       <RatingForm drinkId={drink.id} onSubmit={handleRatingSubmit} />
     </div>
--- my-app/src/components/RatingForm.js
+++ my-app/src/components/RatingForm.js
@@ -2,48 +2,45 @@ import React, { useState } from 'react';
 
 const RatingForm = ({ drinkId, onSubmit }) => {
   const [rating, setRating] = useState(0);
-  const [comment, setComment] = useState('');
 
   const handleRatingChange = (event) => {
     setRating(parseInt(event.target.value, 10));
   };
 
-  const handleCommentChange = (event) => {
-    setComment(event.target.value);
-  };
-
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    // Perform any validation or additional processing before submitting
-
     const newRating = {
       drink_id: drinkId,
-      rating: rating,
-      comment: comment,
+      rating: rating
     };
 
-    // Call the onSubmit function passed from the parent component
-    onSubmit(newRating);
 
-    // Reset the form after submitting
-    setRating(0);
-    setComment('');
+    fetch('http://localhost:9292/ratings', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newRating)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        onSubmit(data);
+        setRating(0);
+      })
+      .catch((error) => {
+        console.error('Error saving rating:', error);
+      });
   };
 
   return (
     <div>
-      <h3>Rate the Drink:</h3>
       <form onSubmit={handleSubmit}>
         <label>
-          Rating:
+          <h3>Rate the Drink: </h3>
           <input type="number" min="1" max="5" value={rating} onChange={handleRatingChange} />
         </label>
-        <label>
-          Comment:
-          <textarea value={comment} onChange={handleCommentChange} />
-        </label>
-        <button type="submit">Submit</button>
+        <button type="submit">Submit Rating</button>
       </form>
     </div>
   );
